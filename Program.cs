8cd8e8db3817a8c8cfda832ac974eec9e7b0c5a5
Uroboros-ji <dimapolschkov@yandex.ru>
2022-02-15 10:24:43 +0300

    тест
    тест

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//namespace ConsoleApp1
+namespace std
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            N += 5;
+            N *= 2;
+            N -= 5;
+            Console.WriteLine("измененное число ");
+            Console.WriteLine(N);
+            Console.WriteLine("Массив:");
+            int[] array = new int[N];
+            
+            for (int j = 0; j < N; j++)
+            {
+                array[j] = r.Next(-100, 101);
+                Console.WriteLine(array[j]);
+            }
+        }
+    }
+}
